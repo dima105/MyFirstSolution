1d9f7d137b2da2180493e6291258d4e4fc583e01
Dmitry_Kulik <dima27121985@gmail.com>
2022-02-20 20:00:59 +0300

    Добавьте файлы проекта.

---
+++ MyFirstSolution/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MyFirstConsole
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int Variable = 10;
+            Console.WriteLine(Variable);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ MySecondConsole/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MySecondConsole
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            double variablePI = 3.141592653;
+            Console.WriteLine(variablePI);
+
+
+
+            decimal variableE = 1.414213562373095048M;
+            Console.WriteLine(variableE);
+        }
+    }
+}
